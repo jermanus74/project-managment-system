4f797d1bd2cdf3f9c9e527211d7a2d0ca2458438
jermanus74 <jermanuspeter@gmail.com>
2025-03-25 23:07:01 +0300

    side bar components

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LockIcon } from "lucide-react";
 import Image from "next/image";
 
 // import React, { useState } from "react";
@@ -15,7 +16,7 @@ const Sidebar = () => {
       <div className="flex h-[100%] w-full flex-col justify-start">
         {/* TOP LOGO */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
-          <div className="text-xl font-bold text-gray-800 dark:text-white">
+          <div className="text-xl font-bold text-gray-800 uppercase dark:text-white">
             PMS
           </div>
         </div>
@@ -23,11 +24,17 @@ const Sidebar = () => {
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-3 dark:border-gray-700">
           <Image height={40} width={40} src="/img/logo.jpeg" alt="logo" />
           <div>
-            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+            <h3 className="text-md font-bold tracking-wide capitalize dark:text-gray-200">
               Heros Team
             </h3>
+            <div className="mt1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500 capitalize">private</p>
+            </div>
           </div>
         </div>
+        {/* navbar links */}
+        <div className=""></div>
       </div>
     </div>
   );
