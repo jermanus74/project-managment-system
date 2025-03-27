5469cebe57582e9ecca418cb126f9444b7ee6e26
jermanus74 <jermanuspeter@gmail.com>
2025-03-27 23:42:10 +0300

    add sidebar collapse logic using useAppSelector in dashboardWrapper

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -3,9 +3,12 @@
 import React from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
-import StoreProvider from "./redux";
+import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  ); //Replace with your actual logic to determine if the sidebar is collapsed
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
